4225581ab81e75c53a5170fbc0e82ef51c955c93
AdamMa <mygamesmylives@gmail.com>
2020-04-24 11:33:42 +0800

    1. delete useless namespaces

---
+++ Pup/MyHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Pup
+{
+    public class MyHandler
+    {
+        private int _flush = 1;
+
+        public void DownloadProgressChangedHandler(object sender, DownloadProgressChangedEventArgs arg)
+        {
+            if (_flush++ % 100 == 0)
+            {
+                var percent = (float) arg.BytesReceived / (float) arg.TotalBytesToReceive;
+                Console.WriteLine(
+                    $"正在下载chrome! Total:{arg.TotalBytesToReceive}B Received:{arg.BytesReceived }B percent:{percent * 100:F}");
+            }
+        }
+    }
+}
--- Pup/Program.cs
+++ Pup/Program.cs
@@ -1,56 +1,82 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.IO;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using CommandLine;
 using PuppeteerSharp;
 
 namespace Pup
 {
     class Program
     {
-        static async Task Main(string[] args)
+        public class Options
         {
-            //var browserFetcher = new BrowserFetcher();
-            //await browserFetcher.DownloadAsync(BrowserFetcher.DefaultRevision);
-            var browser = await Puppeteer.LaunchAsync(new LaunchOptions()
+            [Option('u', "url", Required = true, HelpText = "要访问的网址")]
+            public string Url { get; set; }
+
+            [Option('w', "width", Required = false, HelpText = "设置PDF宽度")]
+            public int Width { get; set; } = 1920;
+
+            [Option('h', "height", Required = false, HelpText = "设置PDF高度")]
+            public int Height { get; set; } = 1080;
+
+            [Option("until", Required = false, HelpText = "设置页面完成事件DOMContentLoaded，Load，Networkidle2，Networkidle0")]
+            public string WaitUntil { get; set; }
+
+            [Option("timeout", Required = false, HelpText = "设置超时时间")] public int Timeout { get; set; } = 3000;
+        }
+
+        public static Task PrintError(IEnumerable<Error> errors)
+        {
+            return Task.CompletedTask;
+        }
+
+        private static async Task DownloadBrowserAsync()
+        {
+            var browserFetcher = new BrowserFetcher();
+            browserFetcher.DownloadProgressChanged += new MyHandler().DownloadProgressChangedHandler;
+            await browserFetcher.DownloadAsync(BrowserFetcher.DefaultRevision);
+        }
+
+        private static async Task<Browser> CreateBrowser(Options option)
+        {
+            return await Puppeteer.LaunchAsync(new LaunchOptions()
             {
                 Headless = true,
                 DefaultViewport = new ViewPortOptions()
                 {
-                    Height = 1080,
-                    Width = 1920,
+                    Height = option.Height,
+                    Width = option.Width,
                 },
                 IgnoreHTTPSErrors = true,
-
             });
-            await using (var page = await browser.NewPageAsync())
-            {
-                await page.GoToAsync("https://www.sogou.com", new NavigationOptions()
-                {
-                    Timeout = 30000,
-                    WaitUntil = new[] { WaitUntilNavigation.Load }
-                });
-                //await page.GoToAsync("https://www.sogou.com", 30000, new []
-                //{
-                //    WaitUntilNavigation.Networkidle0//不再有网络连接时触发
-                //    //WaitUntilNavigation.DOMContentLoaded//页面的DOMContentLoaded触发时
-                //    //WaitUntilNavigation.Networkidle2,//只有两个网络连接时触发
-                //    //WaitUntilNavigation.Load//页面Load页面触发时
-                //}).ConfigureAwait(false);
-                //await page.SetViewportAsync(new ViewPortOptions()
-                //{
-                //    Height = 1920,
-                //    Width = 1920,
-                //});
-                await page.PdfAsync($"{Guid.NewGuid()}.pdf", new PdfOptions()
-                {
-                    Height = 1080,
-                    Width = 1920,
-                    PrintBackground = true,
-                    PreferCSSPageSize = true,
-                });
-            }
+        }
 
+        private static async Task Run(Options option)
+        {
+            await DownloadBrowserAsync();
+            var browser = await CreateBrowser(option);
+            await using var page = await browser.NewPageAsync();
+            await page.GoToAsync(option.Url, new NavigationOptions()
+            {
+                Timeout = option.Timeout,
+                WaitUntil = new[] { Enum.Parse<WaitUntilNavigation>(option.WaitUntil) }
+            });
+
+            await page.PdfAsync($"{Guid.NewGuid()}.pdf", new PdfOptions()
+            {
+                Height = 1080,
+                Width = 1920,
+                PrintBackground = true,
+                PreferCSSPageSize = true,
+            });
+        }
+
+        static async Task Main(string[] args)
+        {
+            var options = Parser.Default.ParseArguments<Options>(args);
+            await options.MapResult(
+                Run,
+                PrintError);
         }
     }
 }
